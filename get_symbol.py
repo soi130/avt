5aae950f0f9d1757ad9872ba7a2e72c819bae89d
yubYern <mynameisnak@gmail.com>
2024-08-04 22:43:21 +0700

    implement awswrangler and parquet format

--- get_symbol.py
+++ get_symbol.py
@@ -3,9 +3,10 @@ from datetime import datetime
 import json
 import boto3
 
-#%% vars
-print('START')
+import awswrangler as wr
+import pandas as pd
 
+#%% vars
 ssm_client = boto3.client('ssm')
 finhub_api_response = ssm_client.get_parameter(
     Name='finnhub_api',
@@ -13,33 +14,30 @@ finhub_api_response = ssm_client.get_parameter(
     )
     
 finhub_api = finhub_api_response['Parameter']['Value']
-
 symbol_bronze_name = "avt-symbol-bronze"
  
 td  = datetime.today()
 y = td.year
 m = td.month
-d = td.day
 
-td_string = f'year={y}/month={m}/'
+td_string = f'year={y}/month={m}'
 
-# %%
 def lambda_handler(event,context):
     s3 = boto3.client('s3')
 
     finnhub_client = finnhub.Client(api_key=finhub_api)
     symbol_json = finnhub_client.stock_symbols('US')
+    print('symbol collected')
+    df = pd.DataFrame(symbol_json)
     
-    name = symbol_bronze_name
-    filename = td_string+'symbol.json'
-    to_s3 = bytes(json.dumps(symbol_json),
-                  encoding='UTF-8')
-
-    s3.put_object(Bucket=symbol_bronze_name,
-                  Key = filename,
-                  Body = to_s3
-                  )
+    wr.s3.to_parquet(df=df,
+        path=f's3://{symbol_bronze_name}/{td_string}/symbol.parquet',
+        index = False,
+        compression = 'snappy',
+        
+    )
+    
     return {"statusCode":200,
-            "log":'Symbol JSON Put to S3 Complete'
+            "log":'Symbol put to S3 Complete'
         
     }
